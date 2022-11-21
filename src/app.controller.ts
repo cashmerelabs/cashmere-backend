3870f2ab023e4910cce3915bbb2fd4fd237ba8c6
pavel.sh <pavel.sh@google.com>
2022-11-21 13:58:38 +0300

    return error message

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import { Controller, Get, HttpException, HttpStatus, Query } from '@nestjs/common';
 import { AppService, ChainID } from './app.service';
 import { IsEthereumAddress, IsIn, IsNumberString } from 'class-validator';
 import axios from 'axios';
@@ -123,104 +123,115 @@ export class AppController {
       toToken,
     }: GetSwapParamsQueryDto,
   ): Promise<object> {
-    console.log({
-      fromChain,
-      fromToken,
-      fromAmount,
-      toChain,
-      toToken,
-    });
-    const fromNetwork = networks[fromChain];
-    const toNetwork = networks[toChain];
-    const pool = fromNetwork.intraChainPool;
-    const poolCC = fromNetwork.l0CrossChainPool;
-    const routerCC = fromNetwork.l0CrossChainRouter;
-    const swapRouter = fromNetwork.l0AggregatorRouter;
-    const { lowestAsset: lwsAssetAddress, highestAsset: hgsAssetAddress } =
-      await pool.getHighestAndLowestCompRatioAssets();
-    const lwsAsset = fromNetwork.assetContract(lwsAssetAddress);
-    const lwsTokenAddress = await lwsAsset.underlyingToken();
-    const hgsAsset = fromNetwork.assetContract(hgsAssetAddress);
-    const hgsTokenAddress = await hgsAsset.underlyingToken();
-    const hgsAssetId = (
-      await routerCC['getAssetData(uint16,address)'](
-        fromNetwork.l0ChainId,
-        hgsAssetAddress,
-      )
-    ).assetId;
-    const assetData = await routerCC['getAssetData(uint16,uint256)'](
-      toNetwork.l0ChainId,
-      hgsAssetId,
-    );
-    const hgsTokenAddressDest = assetData.nativeTokenAddress;
-    const hgsAssetAddressDest = assetData.nativeAssetAddress;
-    const { data } = await axios.get(
-      `https://api.1inch.io/v4.0/${fromChain}/swap`,
-      {
-        params: {
-          fromTokenAddress: fromToken,
-          toTokenAddress: lwsTokenAddress,
-          amount: fromAmount,
-          fromAddress: swapRouter.address,
-          slippage: '1',
-          disableEstimate: true,
-        },
-      },
-    );
-    console.log(data.tx);
-    const swapData = swapRouter.interface.encodeFunctionData(
-      swapRouter.interface.functions[
-        'startSwap((address,uint256,address,address,bytes,address,uint256,address,uint16))'
-      ],
-      [
-        [
-          fromToken,
-          fromAmount,
-          lwsTokenAddress,
-          data.tx.to,
-          data.tx.data,
-          hgsTokenAddressDest,
-          hgsAssetId,
-          toToken,
-          toNetwork.l0ChainId,
-        ],
-      ],
-    );
-    const { potentialOutcome, haircut } = await poolCC.quotePotentialSwap(
-      lwsTokenAddress,
-      hgsTokenAddressDest,
-      data.toTokenAmount,
-      hgsAssetId,
-      toNetwork.l0ChainId,
-    );
-    const estimatePayload = ethers.utils.defaultAbiCoder.encode(
-      [
-        'address',
-        'uint16',
-        'uint16',
-        'address',
-        'address',
-        'uint256',
-        'uint256',
-      ],
-      [
-        swapRouter.address,
-        fromNetwork.l0ChainId,
+    try {
+      console.log({
+        fromChain,
+        fromToken,
+        fromAmount,
+        toChain,
+        toToken,
+      });
+      const fromNetwork = networks[fromChain];
+      const toNetwork = networks[toChain];
+      const pool = fromNetwork.intraChainPool;
+      const poolCC = fromNetwork.l0CrossChainPool;
+      const routerCC = fromNetwork.l0CrossChainRouter;
+      const swapRouter = fromNetwork.l0AggregatorRouter;
+      const { lowestAsset: lwsAssetAddress, highestAsset: hgsAssetAddress } =
+        await pool.getHighestAndLowestCompRatioAssets();
+      const lwsAsset = fromNetwork.assetContract(lwsAssetAddress);
+      const lwsTokenAddress = await lwsAsset.underlyingToken();
+      const hgsAsset = fromNetwork.assetContract(hgsAssetAddress);
+      const hgsTokenAddress = await hgsAsset.underlyingToken();
+      const hgsAssetId = (
+        await routerCC['getAssetData(uint16,address)'](
+          fromNetwork.l0ChainId,
+          hgsAssetAddress,
+        )
+      ).assetId;
+      const assetData = await routerCC['getAssetData(uint16,uint256)'](
         toNetwork.l0ChainId,
-        lwsAsset.address,
-        hgsAssetAddressDest,
-        potentialOutcome,
-        haircut,
-      ],
-    );
-    const value = await routerCC.estimateFee(
-      toNetwork.l0ChainId,
-      estimatePayload,
-    );
-    return {
-      data: swapData,
-      to: swapRouter.address,
-      value,
-    };
+        hgsAssetId,
+      );
+      const hgsTokenAddressDest = assetData.nativeTokenAddress;
+      const hgsAssetAddressDest = assetData.nativeAssetAddress;
+      const { data } = await axios.get(
+        `https://api.1inch.io/v4.0/${fromChain}/swap`,
+        {
+          params: {
+            fromTokenAddress: fromToken,
+            toTokenAddress: lwsTokenAddress,
+            amount: fromAmount,
+            fromAddress: swapRouter.address,
+            slippage: '1',
+            disableEstimate: true,
+          },
+        },
+      );
+      console.log(data.tx);
+      const swapData = swapRouter.interface.encodeFunctionData(
+        swapRouter.interface.functions[
+          'startSwap((address,uint256,address,address,bytes,address,uint256,address,uint16))'
+        ],
+        [
+          [
+            fromToken,
+            fromAmount,
+            lwsTokenAddress,
+            data.tx.to,
+            data.tx.data,
+            hgsTokenAddressDest,
+            hgsAssetId,
+            toToken,
+            toNetwork.l0ChainId,
+          ],
+        ],
+      );
+      const { potentialOutcome, haircut } = await poolCC.quotePotentialSwap(
+        lwsTokenAddress,
+        hgsTokenAddressDest,
+        data.toTokenAmount,
+        hgsAssetId,
+        toNetwork.l0ChainId,
+      );
+      const estimatePayload = ethers.utils.defaultAbiCoder.encode(
+        [
+          'address',
+          'uint16',
+          'uint16',
+          'address',
+          'address',
+          'uint256',
+          'uint256',
+        ],
+        [
+          swapRouter.address,
+          fromNetwork.l0ChainId,
+          toNetwork.l0ChainId,
+          lwsAsset.address,
+          hgsAssetAddressDest,
+          potentialOutcome,
+          haircut,
+        ],
+      );
+      const value = await routerCC.estimateFee(
+        toNetwork.l0ChainId,
+        estimatePayload,
+      );
+      return {
+        data: swapData,
+        to: swapRouter.address,
+        value,
+      };
+    } catch (e) {
+      throw new HttpException(
+        {
+          status: HttpStatus.INTERNAL_SERVER_ERROR,
+          error: 'Internal Server Error',
+          cause: e,
+        },
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
   }
 }
